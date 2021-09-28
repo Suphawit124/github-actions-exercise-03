281fd27510ba2c1e57a678618aea7a10fad0c33e
Suphawit Watthanataetakun <61160124@go.buu.ac.th>
2021-09-28 20:20:57 +0700

    update test

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -3,12 +3,6 @@ const app = require('../app')
 
 describe('GET /', () => {
     it('responds with "Hello world!"', (done) => {
-        request(app).get('/').expect(json({message: 'Hello world!'}), done)
-    })
-})
-
-describe('GET /users', () => {
-    it('responds with "No user in the system"', (done) => {
-        request(app).get('/users').expect('No user in the system', done)
+        request(app).get('/').expect('Hello world!', done)
     })
 })
